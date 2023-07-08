f723d3260f85110235d38bb7b6897f8d3fb8904d
Lucas Sawade <lsawade@princeton.edu>
2023-07-07 20:14:31 -0400

    Updated dell start

--- .bashrc
+++ .bashrc
@@ -157,3 +157,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 alias config='/usr/bin/git --git-dir=/home/lsawade/.cfg/ --work-tree=/home/lsawade'
+
+
--- .start/della.sh
+++ .start/della.sh
@@ -4,3 +4,19 @@ module load anaconda3/2022.5
 module load cudatoolkit/11.7
 module load gcc/8
 module load openmpi/gcc/4.1.2 
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/usr/licensed/anaconda3/2022.5/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/usr/licensed/anaconda3/2022.5/etc/profile.d/conda.sh" ]; then
+        . "/usr/licensed/anaconda3/2022.5/etc/profile.d/conda.sh"
+    else
+        export PATH="/usr/licensed/anaconda3/2022.5/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
